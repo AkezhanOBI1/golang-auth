7e6d8436f24f57c479e26d3fbb17cafedc3cf59a
AkezhanOb1 <31239874+AkezhanOb1@users.noreply.github.com>
2018-06-10 19:38:52 +0900

    Update cookie.go

--- router/cookie.go
+++ router/cookie.go
@@ -16,7 +16,6 @@ func setCookie(w http.ResponseWriter, id string) error{
 	}
 	err = config.Cache.Set(sessionToken.String(), id, time.Hour).Err()
 	if err != nil {
-		// If there is an error in setting the cache, return an internal server error
 		w.WriteHeader(http.StatusInternalServerError)
 		return errors.New("Error Setting cookie")
 	}
@@ -35,10 +34,8 @@ func checkCookie(r *http.Request, id string) (string, error){
 	c, err := r.Cookie(id)
 	if err != nil {
 		if err == http.ErrNoCookie {
-			// If the cookie is not set, return an unauthorized status
 			return  "No Cookie No Party", errors.New("No Cookie")
 		}
-		// For any other type of error, return a bad request status
 		return "Error", errors.New("Error occures")
 	}
 	sessionToken := c.Value
